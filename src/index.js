814c6deacc820bc98b7181b331d84bedda08c726
Moritz Mackiewicz <mma@scherer-software.de>
2018-03-09 13:31:28 +0100

    Fix 'Total: undefined users'

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ module.exports = {
     router.get('/users/count', (req, res) => {
       bp.users.count()
       .then((count) => {
-        res.send(count)
+        res.send(count.toString())
       })
       .catch((err) => res.status(500).send({ message:err.message }))
     })
